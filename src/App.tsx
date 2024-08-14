6f60f4a6865b03e6ff7e7ede4ea18182444f2cdc
Aayush-G18 <goyalaayush292@gmail.com>
2024-08-14 13:15:23 +0530

    added assets,made the initial project structure,made the layout for auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-
+import { Routes,Route } from 'react-router-dom';
 import './globals.css';
+import SignInForm from './_auth/Forms/SignInForm';
+import { Home } from './_root/pages';
+import SignUpForm from './_auth/Forms/SignUpForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 function App() {
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>    </>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public Routes */}
+        <Route element={<AuthLayout/>}>
+          <Route path="/sign-in" element={<SignInForm/>}/>
+          <Route path="/sign-up" element={<SignUpForm/>}/>
+        </Route>
+        {/* secured Routes */}
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home/>}/>
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
