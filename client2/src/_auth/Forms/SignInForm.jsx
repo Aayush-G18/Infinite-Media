4192e0daec70bd5bc04e0d24ba1e93c0e9f564f6
Aayush-G18 <goyalaayush292@gmail.com>
2024-08-16 01:08:19 +0530

    converted frontend to js from ts

---
+++ client2/src/_auth/Forms/SignInForm.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router-dom";
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader } from "@/components/shared/Loader";
+
+// Define the validation schema with Zod
+const SignInValidation = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+const SignInForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm({
+    resolver: zodResolver(SignInValidation),
+    defaultValues: {
+      email: "",
+      password: ""
+    },
+  });
+
+  function onSubmit(values) {
+    // Handle form submission
+    setIsLoading(true);
+    console.log(values);
+    // Simulate an async action
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }
+
+  return (
+    <Form {...form} >
+      <div className="sm:w-420 flex-center flex-col ">
+        <img src="/assets/images/logo.svg" alt="logo" />
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Log in to your account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use Infinite Media, Please enter your details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col  w-full mt-4">
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Email</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="w-full flex-center"><Loader />Loading...</div>
+            ) : "Sign In"}
+          </Button>
+
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Don't have an account?
+            <Link
+              to="/sign-up"
+              className="text-primary-500 text-small-semibold ml-1">
+              Sign Up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
+};
+
+export default SignInForm;
